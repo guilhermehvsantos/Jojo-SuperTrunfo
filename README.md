365a81a071b9ee10435a9e458f0e148ed8d9f9b2
guilhermehvsantos <git@stackblitz.com>
2024-10-03 14:11:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jojo-SuperTrunfo
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/guilhermehvsantos/Jojo-SuperTrunfo)
---
+++ script.js
@@ -0,0 +1,666 @@
+var carta1 = {
+  nome: 'Anubis',
+  imagem:
+    'https://static.jojowiki.com/images/c/c5/latest/20201222083441/Anubis_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/c/cb/latest/20200929010303/8_OVATarot_Anubis.png',
+  atributos: {
+    PWR: 850,
+    SPD: 800,
+    RNG: 200,
+    STA: 850,
+    PRC: 100,
+    DEV: 500,
+  },
+};
+var carta2 = {
+  nome: 'Atum',
+  imagem:
+    'https://static.jojowiki.com/images/b/b6/latest/20191015214914/Ttdarbystand.png',
+  tarot:
+    'https://static.jojowiki.com/images/a/a9/latest/20200929010307/9_OVATarot_Atum.png',
+  atributos: {
+    PWR: 300,
+    SPD: 550,
+    RNG: 350,
+    STA: 750,
+    PRC: 200,
+    DEV: 250,
+  },
+};
+var carta3 = {
+  nome: 'Bastet',
+  imagem:
+    'https://static.jojowiki.com/images/f/f6/latest/20191015215447/Bastet_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/b/b3/latest/20200929010253/4_OVATarot_Bastet.png',
+  atributos: {
+    PWR: 150,
+    SPD: 100,
+    RNG: 800,
+    STA: 850,
+    PRC: 100,
+    DEV: 150,
+  },
+};
+var carta4 = {
+  nome: 'Death',
+  imagem:
+    'https://static.jojowiki.com/images/5/52/latest/20191015215220/Death_Thirteen_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/d/d0/latest/20201003160604/13_OVATarot_Death.png',
+  atributos: {
+    PWR: 550,
+    SPD: 600,
+    RNG: 100,
+    STA: 700,
+    PRC: 250,
+    DEV: 650,
+  },
+};
+var carta5 = {
+  nome: 'Geb',
+  imagem:
+    'https://static.jojowiki.com/images/3/34/latest/20191015214044/Geb_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/7/71/latest/20200929010251/3_OVATarot_Geb.png',
+  atributos: {
+    PWR: 450,
+    SPD: 800,
+    RNG: 900,
+    STA: 700,
+    PRC: 250,
+    DEV: 200,
+  },
+};
+var carta6 = {
+  nome: 'High Priestess',
+  imagem:
+    'https://static.jojowiki.com/images/2/2c/latest/20191015213453/High_Priestess_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/7/7a/latest/20201003160515/2_OVATarot_TheHighPriestess.png',
+  atributos: {
+    PWR: 550,
+    SPD: 700,
+    RNG: 800,
+    STA: 850,
+    PRC: 350,
+    DEV: 300,
+  },
+};
+var carta7 = {
+  nome: 'Horus',
+  imagem:
+    'https://static.jojowiki.com/images/5/59/latest/20191015214708/Horus_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/6/6b/latest/20200929010249/2_OVATarot_Horus.png',
+  atributos: {
+    PWR: 750,
+    SPD: 800,
+    RNG: 350,
+    STA: 550,
+    PRC: 150,
+    DEV: 500,
+  },
+};
+var carta8 = {
+  nome: 'Judgement',
+  imagem:
+    'https://static.jojowiki.com/images/a/af/latest/20191015214313/Judgement_Infobox_Anime.png',
+  tarot:
+    'https://static.jojowiki.com/images/7/77/latest/20201003160631/20_OVATarot_Judgement.png',
+  atributos: {
+    PWR: 800,
+    SPD: 700,
+    RNG: 550,
+    STA: 750,
+    PRC: 350,
+    DEV: 300,
+  },
+};
+var carta9 = {
+  nome: 'Khnum',
+  imagem:
+    'https://static.jojowiki.com/images/a/a1/latest/20191015220100/Khnum_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/f/f0/latest/20191015215010/Khnum_card.png',
+  atributos: {
+    PWR: 100,
+    SPD: 200,
+    RNG: 200,
+    STA: 900,
+    PRC: 150,
+    DEV: 100,
+  },
+};
+var carta10 = {
+  nome: 'Osiris',
+  imagem:
+    'https://static.jojowiki.com/images/7/70/latest/20191015213015/Osiris3.png',
+  tarot:
+    'https://static.jojowiki.com/images/9/9f/latest/20200929010247/1_OVATarot_Osiris.png',
+  atributos: {
+    PWR: 100,
+    SPD: 350,
+    RNG: 400,
+    STA: 550,
+    PRC: 300,
+    DEV: 200,
+  },
+};
+var carta11 = {
+  nome: 'Sethan',
+  imagem:
+    'https://static.jojowiki.com/images/d/de/latest/20191015214707/Sethan_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/9/90/latest/20200929010256/5_OVATarot_Sethan.png',
+  atributos: {
+    PWR: 150,
+    SPD: 350,
+    RNG: 200,
+    STA: 600,
+    PRC: 200,
+    DEV: 100,
+  },
+};
+var carta12 = {
+  nome: 'Thoth',
+  imagem:
+    'https://static.jojowiki.com/images/c/c8/latest/20191015215812/Begone_thoth.gif',
+  tarot:
+    'https://static.jojowiki.com/images/6/6b/latest/20200929010301/7_OVATarot_Thoth.png',
+  atributos: {
+    PWR: 150,
+    SPD: 200,
+    RNG: 200,
+    STA: 850,
+    PRC: 150,
+    DEV: 100,
+  },
+};
+var carta13 = {
+  nome: 'Dark Blue Moon',
+  imagem:
+    'https://static.jojowiki.com/images/f/fc/latest/20191015214306/Dark_Blue_Moon_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/7/74/latest/20201003160623/18_OVATarot_TheMoon.png',
+  atributos: {
+    PWR: 550,
+    SPD: 600,
+    RNG: 750,
+    STA: 700,
+    PRC: 400,
+    DEV: 350,
+  },
+};
+var carta13 = {
+  nome: 'Emperor',
+  imagem:
+    'https://static.jojowiki.com/images/5/5d/latest/20210324233551/Emperor_Infobox_Anime.png',
+  tarot:
+    'https://static.jojowiki.com/images/2/26/latest/20201003160526/4_OVATarot_TheEmperor.png',
+  atributos: {
+    PWR: 800,
+    SPD: 800,
+    RNG: 750,
+    STA: 550,
+    PRC: 200,
+    DEV: 100,
+  },
+};
+var carta14 = {
+  nome: 'Ebony Devil',
+  imagem:
+    'https://static.jojowiki.com/images/f/f3/latest/20191015214019/Ebony_Devil_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/e/ea/latest/20201003160612/15_OVATarot_TheDevil.png',
+  atributos: {
+    PWR: 350,
+    SPD: 300,
+    RNG: 850,
+    STA: 700,
+    PRC: 350,
+    DEV: 650,
+  },
+};
+var carta15 = {
+  nome: 'Empress',
+  imagem:
+    'https://static.jojowiki.com/images/5/59/latest/20210607101545/Empress_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/e/ed/latest/20201003160520/3_OVATarot_TheEmpress.png',
+  atributos: {
+    PWR: 550,
+    SPD: 100,
+    RNG: 850,
+    STA: 800,
+    PRC: 200,
+    DEV: 150,
+  },
+};
+var carta16 = {
+  nome: 'Hanged Man',
+  imagem:
+    'https://static.jojowiki.com/images/2/26/latest/20191015212959/Hanged_Man_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/3/3d/latest/20201003160601/12_OVATarot_TheHangedMan.png',
+  atributos: {
+    PWR: 500,
+    SPD: 850,
+    RNG: 900,
+    STA: 600,
+    PRC: 300,
+    DEV: 350,
+  },
+};
+var carta17 = {
+  nome: 'Hermit Purple',
+  imagem:
+    'https://static.jojowiki.com/images/c/cf/latest/20191015212829/Hermit_purple.gif',
+  tarot:
+    'https://static.jojowiki.com/images/8/8e/latest/20201003160547/9_OVATarot_TheHermit.png',
+  atributos: {
+    PWR: 350,
+    SPD: 550,
+    RNG: 400,
+    STA: 800,
+    PRC: 300,
+    DEV: 100,
+  },
+};
+var carta18 = {
+  nome: 'Hierophant Green',
+  imagem:
+    'https://static.jojowiki.com/images/2/2a/latest/20191020224607/Hierophant_green_powa_2.gif',
+  tarot:
+    'https://static.jojowiki.com/images/7/7f/latest/20201003160530/5_OVATarot_TheHierophant.png',
+  atributos: {
+    PWR: 550,
+    SPD: 750,
+    RNG: 900,
+    STA: 800,
+    PRC: 450,
+    DEV: 300,
+  },
+};
+var carta19 = {
+  nome: 'Justice',
+  imagem:
+    'https://static.jojowiki.com/images/d/d2/latest/20210710193909/Justice_Appearance.png',
+  tarot:
+    'https://static.jojowiki.com/images/a/a5/latest/20201003160557/11_OVATarot_Justice.png',
+  atributos: {
+    PWR: 350,
+    SPD: 150,
+    RNG: 900,
+    STA: 850,
+    PRC: 150,
+    DEV: 200,
+  },
+};
+var carta20 = {
+  nome: 'Lovers',
+  imagem:
+    'https://static.jojowiki.com/images/c/c4/latest/20191015213758/Lovers_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/9/92/latest/20201003160534/6_OVATarot_TheLover.png',
+  atributos: {
+    PWR: 150,
+    SPD: 350,
+    RNG: 850,
+    STA: 900,
+    PRC: 400,
+    DEV: 250,
+  },
+};
+var carta21 = {
+  nome: "Magician's Red",
+  imagem:
+    'https://static.jojowiki.com/images/6/68/latest/20200517085750/Magician_red_detector.gif',
+  tarot:
+    'https://static.jojowiki.com/images/2/28/latest/20201003160511/1_OVATarot_TheMagician.png',
+  atributos: {
+    PWR: 800,
+    SPD: 800,
+    RNG: 550,
+    STA: 750,
+    PRC: 550,
+    DEV: 350,
+  },
+};
+var carta22 = {
+  nome: 'Silver Chariot',
+  imagem: 'https://thumbs.gfycat.com/EasygoingWastefulHake-max-1mb.gif',
+  tarot:
+    'https://static.jojowiki.com/images/4/4f/latest/20201003160538/7_OVATarot_TheChariot.png',
+  atributos: {
+    PWR: 650,
+    SPD: 950,
+    RNG: 500,
+    STA: 800,
+    PRC: 750,
+    DEV: 550,
+  },
+};
+var carta23 = {
+  nome: 'Star Platinum',
+  imagem:
+    'http://pa1.narvii.com/6399/2fa112c05dcde3025416fba567f3a34704e4f1a9_00.gif',
+  tarot:
+    'https://static.jojowiki.com/images/0/0c/latest/20201003160619/17_OVATarot_TheStar.png',
+  atributos: {
+    PWR: 999,
+    SPD: 999,
+    RNG: 550,
+    STA: 999,
+    PRC: 999,
+    DEV: 999,
+  },
+};
+var carta24 = {
+  nome: 'Strength',
+  imagem:
+    'https://static.jojowiki.com/images/9/98/latest/20191015214246/Strength_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/b/b7/latest/20201003160543/8_OVATarot_Strength.png',
+  atributos: {
+    PWR: 800,
+    SPD: 350,
+    RNG: 400,
+    STA: 900,
+    PRC: 100,
+    DEV: 150,
+  },
+};
+var carta25 = {
+  nome: 'Sun',
+  imagem:
+    'https://static.jojowiki.com/images/d/d8/latest/20191015220014/Sun_Desert.png',
+  tarot:
+    'https://static.jojowiki.com/images/5/57/latest/20201003160628/19_OVATarot_TheSun.png',
+  atributos: {
+    PWR: 800,
+    SPD: 250,
+    RNG: 900,
+    STA: 850,
+    PRC: 200,
+    DEV: 100,
+  },
+};
+var carta26 = {
+  nome: 'The Fool',
+  imagem:
+    'https://static.jojowiki.com/images/6/61/latest/20191020090633/The_fool_powa.gif',
+  tarot:
+    'https://static.jojowiki.com/images/3/34/latest/20201003160506/0_OVATarot_TheFool.png',
+  atributos: {
+    PWR: 750,
+    SPD: 550,
+    RNG: 350,
+    STA: 550,
+    PRC: 350,
+    DEV: 600,
+  },
+};
+var carta27 = {
+  nome: 'The World',
+  imagem: 'https://thumbs.gfycat.com/DeliriousHappyGaur-size_restricted.gif',
+  tarot:
+    'https://static.jojowiki.com/images/f/fd/latest/20201003160635/21_OVATarot_TheWorld.png',
+  atributos: {
+    PWR: 990,
+    SPD: 990,
+    RNG: 350,
+    STA: 990,
+    PRC: 990,
+    DEV: 990,
+  },
+};
+var carta28 = {
+  nome: 'The Tower of Gray',
+  imagem:
+    'https://static.jojowiki.com/images/6/6e/latest/20210313134400/Tower_of_Gray_Infobox_Manga.png',
+  tarot:
+    'https://static.jojowiki.com/images/1/10/latest/20201003160615/16_OVATarot_TheTower.png',
+  atributos: {
+    PWR: 150,
+    SPD: 850,
+    RNG: 800,
+    STA: 550,
+    PRC: 200,
+    DEV: 150,
+  },
+};
+var carta29 = {
+  nome: 'The Wheel of Fortune',
+  imagem:
+    'https://static.jojowiki.com/images/8/85/latest/20210714122022/Wheel_of_Fortune_appearance.png',
+  tarot:
+    'https://static.jojowiki.com/images/0/0e/latest/20201003160552/10_OVATarot_WheelOfFortune.png',
+  atributos: {
+    PWR: 700,
+    SPD: 400,
+    RNG: 350,
+    STA: 900,
+    PRC: 200,
+    DEV: 300,
+  },
+};
+var carta30 = {
+  nome: 'Yellow Temperance',
+  imagem:
+    'https://static.jojowiki.com/images/a/a4/latest/20210710193318/Yellow_Temperance_Appearance.png',
+  tarot:
+    'https://static.jojowiki.com/images/a/a2/latest/20201003160609/14_OVATarot_Temperance.png',
+  atributos: {
+    PWR: 400,
+    SPD: 550,
+    RNG: 150,
+    STA: 850,
+    PRC: 350,
+    DEV: 200,
+  },
+};
+var carta31 = {
+  nome: 'Jolyne Cujoh',
+  imagem:
+    'https://criticalhits.com.br/wp-content/uploads/2021/08/d0f-910x512.jpg',
+  atributos: {
+    PWR: '- - -',
+    SPD: '- - -',
+    RNG: '- - -',
+    STA: '- - -',
+    PRC: '- - -',
+    DEV: '- - -',
+  },
+};
+
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22,
+  carta23,
+  carta24,
+  carta25,
+  carta26,
+  carta27,
+  carta28,
+  carta29,
+  carta30,
+  carta31,
+];
+
+var cartaOponente;
+var cartaJogador;
+
+var placar = [0, 0];
+exibirPlacar();
+
+function sortearCarta() {
+  var numeroCartaJogador = parseInt(Math.random() * 31);
+  cartaJogador = cartas[numeroCartaJogador];
+
+  var numeroCartaOponente = parseInt(Math.random() * 31);
+  while (numeroCartaJogador == numeroCartaOponente) {
+    numeroCartaOponente = parseInt(Math.random() * 31);
+  }
+  cartaOponente = cartas[numeroCartaOponente];
+
+  document.getElementById('btnSortear').disabled = true;
+  document.getElementById('btnJogar').disabled = false;
+  document.getElementById('btnNova').disabled = true;
+
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName('atributo');
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var elementoResultado = document.getElementById('resultado');
+  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+  var valorCartaOponente = cartaOponente.atributos[atributoSelecionado];
+  var pontoJogador = document.getElementById('ponto-jogador');
+  var pontoOponente = document.getElementById('ponto-oponente');
+
+  exibirCartaOponente();
+
+  if (valorCartaJogador > valorCartaOponente) {
+    placar[0]++;
+    elementoResultado.innerHTML = 'Você Venceu!';
+  } else if (valorCartaJogador < valorCartaOponente) {
+    placar[1]++;
+    elementoResultado.innerHTML = 'Você Perdeu!';
+  } else if (cartaJogador == cartas[30] && cartaOponente == cartas[22]) {
+    placar[0]++;
+    elementoResultado.innerHTML =
+      'Você Venceu! Seu pai não consegue lutar contra sua filha';
+  } else if (cartaJogador == cartas[22] && cartaOponente == cartas[30]) {
+    placar[1]++;
+    elementoResultado.innerHTML = 'Você Perdeu! Sua filha é sua maior fraqueza';
+  } else if (cartaJogador == cartas[30]) {
+    placar[1]++;
+    elementoResultado.innerHTML =
+      'Você Perdeu! Jolyne ainda não sabe usar seu Stand!';
+  } else if (cartaOponente == cartas[30]) {
+    placar[0]++;
+    elementoResultado.innerHTML =
+      'Você Venceu! Jolyne ainda não sabe usar seu Stand!';
+  } else {
+    elementoResultado.innerHTML = 'Empatou!';
+  }
+
+  document.getElementById('btnSortear').disabled = true;
+  document.getElementById('btnJogar').disabled = true;
+  document.getElementById('btnNova').disabled = false;
+
+  pontoJogador.innerHTML = placar[0];
+  pontoOponente.innerHTML = placar[1];
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById('carta-jogador');
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  var moldura =
+    '<img src="https://i.ibb.co/5vd6cqg/cardtemplate.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = '';
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      ' ' +
+      "<label for'" +
+      atributo +
+      "><input type='radio' name='atributo' id='" +
+      atributo +
+      "'value='" +
+      atributo +
+      "'>" +
+      atributo +
+      ' ' +
+      cartaJogador.atributos[atributo] +
+      '</label>';
+  }
+
+  var nome = `<p class="carta-subtitle">  ${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + '</div>';
+}
+
+function exibirCartaOponente() {
+  var divCartaOponente = document.getElementById('carta-oponente');
+  divCartaOponente.style.backgroundImage = `url(${cartaOponente.imagem})`;
+  var moldura =
+    '<img src="https://i.ibb.co/5vd6cqg/cardtemplate.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div  id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = '';
+  for (var atributo in cartaOponente.atributos) {
+    opcoesTexto +=
+      ' ' +
+      "<label for'" +
+      atributo +
+      "><input disabled type='radio' name='atributo' id='" +
+      atributo +
+      "'value='" +
+      atributo +
+      "'>" +
+      atributo +
+      ' ' +
+      cartaOponente.atributos[atributo] +
+      '</label>';
+  }
+  var nome = `<p class="carta-subtitle">  ${cartaOponente.nome}</p>`;
+
+  divCartaOponente.innerHTML =
+    moldura + nome + tagHTML + opcoesTexto + '</div>';
+}
+
+function novaRodada() {
+  var atrasOponente = document.getElementById('carta-oponente');
+  atrasOponente.innerHTML = `<img src="https://i.ibb.co/XxdXvJW/backcard.pngg" style=" width: inherit; height: inherit; position: absolute;">`;
+
+  var atrasJogador = document.getElementById('carta-jogador');
+  atrasJogador.innerHTML = `<img src="https://i.ibb.co/XxdXvJW/backcard.pngg" style=" width: inherit; height: inherit; position: absolute;">`;
+
+  document.getElementById('btnSortear').disabled = false;
+  document.getElementById('btnNova').disabled = true;
+
+  var limparResultado = document.getElementById('resultado');
+  limparResultado.innerHTML = '';
+}
+
+function exibirPlacar() {
+  var somaPontoJogador = document.getElementById('ponto-jogador');
+  var somaPontoOponente = document.getElementById('ponto-oponente');
+
+  somaPontoJogador.innerHTML = placar[0];
+  somaPontoOponente.innerHTML = placar[1];
+}
